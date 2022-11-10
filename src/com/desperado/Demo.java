71fdaccd195ac21c0b8e86af60282c03d622206c
desperado <17805272076@163.com>
2022-11-10 11:31:44 +0800

    线程终止 --- 不正确的线程终止 -stop

---
+++ src/com/desperado/Demo.java
@@ -0,0 +1,44 @@
+package com.desperado;
+
+/**
+ * 线程终止 --- 不正确的线程终止 -stop
+ */
+public class Demo {
+    int i = 0;
+    int j = 0;
+    public static void main(String[] args) {
+
+        StopThread  thread = new StopThread();
+        thread.start();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        //终止线程
+        thread.stop();
+        thread.print();
+
+
+    }
+}
+class StopThread extends  Thread{
+    private int i=0;
+    private int j=0;
+    @Override
+    public void run() {
+        synchronized (this) {
+            i++;
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            j++;
+        }
+    }
+    public  void print(){
+        System.out.println("i========="+i);
+        System.out.println("j========="+j);
+    }
+}
