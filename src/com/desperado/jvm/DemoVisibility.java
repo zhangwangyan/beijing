a437427ee0c649010d16f391bcf28e946a795547
desperado <17805272076@163.com>
2022-11-12 11:35:39 +0800

    A-可见性问题。

---
+++ src/com/desperado/jvm/DemoVisibility.java
@@ -0,0 +1,29 @@
+package com.desperado.jvm;
+
+/**             32位jdk       64位jdk
+ *  -server     不打印i的值    不打印i的值
+ *  -client     打印i的值     不打印i的值
+ */
+public class DemoVisibility {
+    int i=0;
+    public boolean isRunning =true;
+
+    public static void main(String[] args) throws InterruptedException {
+        DemoVisibility demoVisibility = new DemoVisibility();
+        new Thread(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        System.out.println("你好，desperado");
+                        while(demoVisibility.isRunning){
+                            demoVisibility.i++;
+                        }
+                        System.out.println(demoVisibility.i);
+                    }
+                }
+        ).start();
+        Thread.sleep(3000L);
+        demoVisibility.isRunning=false;
+        System.out.println("shutdown...");
+    }
+}
---
+++ src/com/desperado/jvm/package-info.java
@@ -0,0 +1,4 @@
+package com.desperado.jvm;
+/*
+* java内存模型 、多线程基础知识点
+* */
