81b4350f18a6cc2b065da27ad66394d950a8cc4a
desperado <17805272076@163.com>
2022-11-18 15:01:59 +0800

    A-队列

---
+++ src/com/desperado/list_set_queue/queue/TestConcurrentLinkedQueue.java
@@ -0,0 +1,10 @@
+package com.desperado.list_set_queue.queue;
+
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class TestConcurrentLinkedQueue {
+    public static void main(String[] args) {
+        ConcurrentLinkedQueue<Object> objects = new ConcurrentLinkedQueue<>();
+        objects.
+    }
+}
---
+++ src/com/desperado/list_set_queue/queue/TestLinkedBlockQueue.java
@@ -0,0 +1,10 @@
+package com.desperado.list_set_queue.queue;
+
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class TestLinkedBlockQueue {
+    public static void main(String[] args) {
+        LinkedBlockingQueue<Object> objects = new LinkedBlockingQueue<>();
+
+    }
+}
