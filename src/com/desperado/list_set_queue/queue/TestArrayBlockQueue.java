fa3f94b3e23a28b23da668d7fa569a34f6e73689
desperado <17805272076@163.com>
2022-11-18 09:04:18 +0800

    A-ArrayBlockingQueue

---
+++ src/com/desperado/list_set_queue/queue/TestArrayBlockQueue.java
@@ -0,0 +1,11 @@
+package com.desperado.list_set_queue.queue;
+
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class TestArrayBlockQueue {
+    public static void main(String[] args) {
+        ArrayBlockingQueue arrayBlockingQueue = new ArrayBlockingQueue<Object>(3);
+        //循环数组，用数组实现队列算法
+
+    }
+}
